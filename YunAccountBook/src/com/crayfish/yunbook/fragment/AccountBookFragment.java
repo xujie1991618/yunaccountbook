74bf063b97ea406be3badf5bf1020d195aa8db4d
Crayfish <1159911918@qq.com>
2015-05-21 23:26:32 +0800

    布局加入listview中

--- YunAccountBook/src/com/crayfish/yunbook/fragment/AccountBookFragment.java
+++ YunAccountBook/src/com/crayfish/yunbook/fragment/AccountBookFragment.java
@@ -5,11 +5,8 @@
  */
 package com.crayfish.yunbook.fragment;
 
-import java.util.Locale;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.SectionIndexer;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,11 +34,15 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
 
 	static class SimpleAdapter extends ArrayAdapter<Item> implements PinnedSectionListAdapter {
 
+		private LayoutInflater inflater;
+		
         private static final int[] COLORS = new int[] {
-            R.color.lightblue };
+        	R.color.green_light, R.color.orange_light,
+            R.color.blue_light, R.color.red_light};
 
         public SimpleAdapter(Context context, int resource, int textViewResourceId) {
             super(context, resource, textViewResourceId);
+            this.inflater = LayoutInflater.from(context);
             generateDataset('A', 'Z', false);
         }
 
@@ -53,7 +55,7 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
 
             int sectionPosition = 0, listPosition = 0;
             for (char i=0; i<sectionsNumber; i++) {
-                Item section = new Item(Item.SECTION, String.valueOf((char)('A' + i)));
+                Item section = new Item(Item.SECTION, "1","2","3","4");
                 section.sectionPosition = sectionPosition;
                 section.listPosition = listPosition++;
                 onSectionAdded(section, sectionPosition);
@@ -61,7 +63,7 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
 
                 final int itemsNumber = (int) Math.abs((Math.cos(2f*Math.PI/3f * sectionsNumber / (i+1f)) * 25f));
                 for (int j=0;j<itemsNumber;j++) {
-                    Item item = new Item(Item.ITEM, section.text.toUpperCase(Locale.ENGLISH) + " - " + j);
+                    Item item = new Item(Item.ITEM, "a","b","d","e");
                     item.sectionPosition = sectionPosition;
                     item.listPosition = listPosition++;
                     add(item);
@@ -75,15 +77,52 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
         protected void onSectionAdded(Item section, int sectionPosition) { }
 
         @Override public View getView(int position, View convertView, ViewGroup parent) {
-            TextView view = (TextView) super.getView(position, convertView, parent);
-            view.setTextColor(Color.DKGRAY);
-            view.setTag("" + position);
-            Item item = getItem(position);
-            if (item.type == Item.SECTION) {
-                //view.setOnClickListener(PinnedSectionListActivity.this);
-                view.setBackgroundColor(parent.getResources().getColor(COLORS[item.sectionPosition % COLORS.length]));
-            }
-            return view;
+//            TextView view = (TextView) super.getView(position, convertView, parent);
+//            view.setTextColor(Color.DKGRAY);
+//            view.setTag("" + position);
+//            Item item = getItem(position);
+//            if (item.type == Item.SECTION) {
+//                //view.setOnClickListener(PinnedSectionListActivity.this);
+//                view.setBackgroundColor(parent.getResources().getColor(COLORS[item.sectionPosition % COLORS.length]));
+//            }
+        	Item item = getItem(position);
+        	if(item.type == Item.SECTION){
+        		ViewHolderTitle titleHolder;
+	        	if(convertView ==null){
+	        		convertView = inflater.inflate(R.layout.accountbook_title_item, null);
+	        		titleHolder = new ViewHolderTitle();
+	        		titleHolder.date_textview = (TextView)convertView.findViewById(R.id.date);
+	        		titleHolder.expend_total_textview = (TextView)convertView.findViewById(R.id.expend_total);
+	        		titleHolder.income_total_textview = (TextView)convertView.findViewById(R.id.income_total);
+	        		titleHolder.record_count_textview = (TextView)convertView.findViewById(R.id.record_count);
+	        		convertView.setTag(titleHolder);//绑定对象
+	        	}else{
+	        		titleHolder = (ViewHolderTitle)convertView.getTag();
+	        	}
+//	        	titleHolder.date_textview.setText(item.text1);
+//	        	titleHolder.expend_total_textview.setText(item.text2);
+//	        	titleHolder.income_total_textview.setText(item.text3);
+//	        	titleHolder.record_count_textview.setText(item.text4);
+	        	convertView.setBackgroundColor(parent.getResources().getColor(COLORS[item.sectionPosition % COLORS.length]));
+        	}else{
+        		ViewHolderItem itemHolder;
+        		if(convertView == null){
+        			convertView = inflater.inflate(R.layout.accountbook_item, null);
+	        		itemHolder = new ViewHolderItem();
+	        		itemHolder.item_icon_Image = (ImageView)convertView.findViewById(R.id.item_icon);
+	        		itemHolder.timeandtype_textview = (TextView)convertView.findViewById(R.id.timeandtype);
+	        		itemHolder.type_name_textview = (TextView)convertView.findViewById(R.id.type_name);
+	        		itemHolder.money_textview = (TextView)convertView.findViewById(R.id.money);
+	        		convertView.setTag(itemHolder);//绑定对象
+	        	}else{
+	        		itemHolder = (ViewHolderItem)convertView.getTag();
+	        	}
+//        		itemHolder.item_icon_Image.setImageResource(R.drawable.ic_launcher);
+//        		itemHolder.timeandtype_textview.setText(item.text2);
+//        		itemHolder.type_name_textview.setText(item.text3);
+//        		itemHolder.money_textview.setText(item.text4);
+        	}
+            return convertView;
         }
 
         @Override public int getViewTypeCount() {
@@ -98,7 +137,18 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
         public boolean isItemViewTypePinned(int viewType) {
             return viewType == Item.SECTION;
         }
-
+        class ViewHolderTitle{
+        	TextView date_textview;
+        	TextView expend_total_textview;
+        	TextView income_total_textview;
+        	TextView record_count_textview;
+        }
+        class ViewHolderItem{
+        	ImageView item_icon_Image;
+        	TextView type_name_textview;
+        	TextView timeandtype_textview;
+        	TextView money_textview;
+        }
     }
 	
 	static class FastScrollAdapter extends SimpleAdapter implements SectionIndexer {
@@ -143,18 +193,24 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
 		public static final int SECTION = 1;
 
 		public final int type;
-		public final String text;
+		public final String text1;
+		public final String text2;
+		public final String text3;
+		public final String text4;
 
 		public int sectionPosition;
 		public int listPosition;
 
-		public Item(int type, String text) {
+		public Item(int type, String text1,String text2,String text3,String text4) {
 		    this.type = type;
-		    this.text = text;
+		    this.text1 = text1;
+		    this.text2 = text2;
+		    this.text3 = text3;
+		    this.text4 = text4;
 		}
 
 		@Override public String toString() {
-			return text;
+			return text1;
 		}
 
 	}
@@ -195,7 +251,7 @@ public class AccountBookFragment extends Fragment implements OnClickListener{
 				// TODO Auto-generated method stub
 				Item item = (Item) accountbookListview.getAdapter().getItem(position);
 			    if (item != null) {
-			        Toast.makeText(getActivity(), "Item " + position + ": " + item.text, Toast.LENGTH_SHORT).show();
+			        Toast.makeText(getActivity(), "Item " + position + ": " + item.text1, Toast.LENGTH_SHORT).show();
 			    } else {
 			        Toast.makeText(getActivity(), "Item " + position, Toast.LENGTH_SHORT).show();
 			    }
